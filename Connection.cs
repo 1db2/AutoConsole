97183f253bad22f6effb5bff6c34b73e326eecf2
kukac <labamkozottvanegy@gmail.com>
2024-09-23 11:16:36 +0200

    Connection class

---
+++ Connection.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Text;
+using System.Linq;
+using System.Data.Common;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using System.Collections.Generic;
+
+namespace AutoConsole
+{
+    internal class Connection
+    {
+        public MySqlConnection connection;
+        public string Host;
+        public string Database;
+        public string User;
+        public string Password;
+        public string ConnectionString;
+
+        public Connection()
+        {
+            Host = "127.0.0.1";
+            Database = "Cars";
+            User = "root";
+            Password = "";
+
+            ConnectionString = "SERVER=" + Host + ";DATABASE=" + Database + ";UID=" + User + ";PASSWORD=" + Password + ";SslMode=None";
+
+            connection = new MySqlConnection(ConnectionString);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 
 namespace AutoConsole
 {
+
     internal class Program
     {
         static void Main(string[] args)
